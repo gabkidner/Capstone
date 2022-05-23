eb0e8058b1f9e695246a16a628aa528783d8047c
gabkidner <gabrielkidner@mail.weber.edu>
2022-05-23 12:43:57 -0600

    v

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,1 +1,16 @@
-
+public class TicTacToe{
+  private char mySymbol;
+  private char userSymbol;
+  public TicTacToe(char playerSymbol){
+    userSymbol = playerSymbol;
+    if(playerSymbol == 'x'){
+      mySymbol = 'o';
+    }
+    else{
+      mySymbol = 'x';
+    }
+  }
+  public void game(int space){
+    
+  }
+}
