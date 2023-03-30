57b225d3ff40f36f930dd6732954b3286e890a16
GabKid <89548086+gabkidner@users.noreply.github.com>
2023-03-30 13:08:38 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "TwentyFortyEight.h"
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+//Figure out Press Enter to Continue
+
+main(){
+  TwentyFortyEight twenty;
+  cout << twenty;
+}
