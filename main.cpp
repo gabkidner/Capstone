5174b41c2b607837b33e1ed02d15722800a144ca
gabkidner <gkidner07@gmail.com>
2023-11-15 12:23:43 -0700

    Worked on Story

--- main.cpp
+++ main.cpp
@@ -1,28 +1,8 @@
-#include "TicTacToe.h"
+#include "ChooseAdventure.h"
 #include <iostream>
 using namespace std;
 
 main(){
-  int choiceInt;
-  char choiceChar;
-  TicTacToe tic;
-  while(tic.won() == 0){
-    tic.switchPlayer();
-    system("cls");
-    cout << tic << "\n" << tic.get() << "'s turn\n";
-    cin >> choiceInt;
-    while(choiceInt > 9){
-      cout << "Please enter a valid number\n";
-      cin >> choiceInt;
-    }
-    choiceChar = choiceInt + 48;
-    tic.place(choiceChar);
-  }
-  cout << tic << endl;
-  if(tic.won() == 1){
-    cout << tic.get() << "'s win";
-  }
-  else{
-    cout << "Neither of you won, you are equals" << endl;
-  }
+  ChooseAdventure c;
+  c.printStory();
 }
